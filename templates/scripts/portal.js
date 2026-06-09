a3ec2a083cf46d087622563e2201ae4f8a6e4022
andrewdp <andrew.depeiza@cra-arc.gc.ca>
2026-06-09 13:36:46 -0400

    Add stepper component and it's to quartz template

--- templates/scripts/portal.js
+++ templates/scripts/portal.js
@@ -1,12 +1,12 @@
 "use strict";
 
-var anchorUri,
+let anchorUri,
     anchorEl,
     expandHideBtn,
-    accordions, 
     quartzSideNav = document.getElementsByTagName("quartz-sidenav"),
     navLink = quartzSideNav[0].getElementsByTagName("a"),
     contentSection = document.getElementsByTagName("mat-drawer-content"),
+    accordions = contentSection[0].getElementsByTagName("mat-expansion-panel"),
     sideNav = document.querySelector("mat-sidenav"),
     accountBtn = document.querySelector(".quartz-primary-button"),
     sdMenuBtn = document.getElementsByTagName("quartz-icon-button"),
@@ -14,7 +14,8 @@ var anchorUri,
     printBtns = document.querySelectorAll("button[value='print']"),
     linkBtns = document.querySelectorAll("button[value^='http']"),
     tabs = document.querySelectorAll("[role='tab']"),
-    dialogs = document.querySelectorAll("button[popovertarget]"),
+    dialogs = document.querySelectorAll("button[popovertarget]"), 
+    stepContainer = document.querySelectorAll(".mat-vertical-content-container.mat-stepper-vertical-line"), 
     accordionActivate = function () {
         let matPanel = this.closest("mat-expansion-panel"),
             matIcon = matPanel.querySelector("mat-icon"),
@@ -90,7 +91,7 @@ var anchorUri,
             sideNav = document.querySelector("mat-sidenav"),
             backdropEl = document.querySelector(".mat-drawer-backdrop");
 
-        if (showMenu !== true && sideNav.classList.contains("mat-drawer-opened") === true) {
+        if (showMenu !== true && sideNav.classList.contains("mat-drawer-opened") === true && document.querySelector("mat-sidenav-content") !== null) {
             //Hide side navigation
             sideNavContain.classList.remove("mat-drawer-container-has-open");
             sideNav.classList.remove("mat-drawer-opened");
@@ -104,7 +105,7 @@ var anchorUri,
             //Show side navigation
             sideNavContain.classList.add("mat-drawer-container-has-open");
             sideNav.classList.add("mat-drawer-opened");
-            if (window.innerWidth < 960) {
+            if (globalThis.innerWidth < 960 && document.querySelector("mat-sidenav-content") !== null) {
                 backdropEl.classList.add("mat-drawer-shown");
                 backdropEl.addEventListener("click", hideSideNav, false);
             }
@@ -112,74 +113,96 @@ var anchorUri,
             sideNav.style.boxShadow = "";
             sideNav.style.visibility = "visible";
         }
-    },
+    }, 
     reSizeAction = function () {
         let backdropEl = document.querySelector(".mat-drawer-backdrop"),
             altLangLnk = document.querySelector("[lang='fr']"),
-            showHideNav = function (hideSideNav) {
-                var sideMenuIcon = document.querySelector("quartz-icon-button"),
+            smallPageView = function (hideSideNav) {
+                let footEl;
+                const tabGroups = document.querySelectorAll("mat-tab-header"), 
+                    sideMenuIcon = document.querySelector("quartz-icon-button"),
                     sideNavContain = document.querySelector("mat-sidenav-container"),
                     sideNav = document.querySelector("mat-sidenav"),
                     sideNavContent = document.querySelector("mat-sidenav-content"),
                     ribbonTitle = document.querySelector(".quartz-ribbon-menu-title"),
                     ribbonSubTitle = document.querySelector(".quartz-ribbon-menu-subtitle"),
-                    footEl = document.querySelector(".footer-down, .footer-mobile.down"),
                     footLink = document.querySelector(".link-wrap");
 
                 if (hideSideNav === true) {
                     // large screen UI adjustments
-                    sideMenuIcon.classList.add("quartz-invisible");
                     sideNavContain.classList.remove("mat-drawer-transition");
-                    sideNav.classList.add("mat-drawer-side");
-                    sideNav.classList.remove("mat-drawer-over");
-                    if (sideNavContent.style.getPropertyPriority("margin-left") !== "important") {
+                    if (sideNavContent !== null) {
+                        sideMenuIcon.classList.add("quartz-invisible");
                         sideNavContent.style.marginLeft = "280px";
+                        sideNav.classList.add("mat-drawer-side");
+                        sideNav.classList.remove("mat-drawer-over");
+                        sideNav.classList.add("mat-drawer-opened");
                     }
                     ribbonTitle.classList.remove("quartz-invisible");
-                    ribbonSubTitle.classList.remove("quartz-invisible");
-                    footEl.classList.remove("column-count-one");
+                    if (ribbonSubTitle !== null) {
+                        ribbonSubTitle.classList.remove("quartz-invisible");
+                    }
                     footLink.classList.add("footer-down");
                     footLink.classList.remove("footer-mobile-down");
-                    sideNav.classList.add("mat-drawer-opened");
+                    footEl = document.querySelector(".footer-down, .footer-mobile.down");
+                    footEl.classList.remove("column-count-one");
                     showHideMenu(true);
                 } else {
                     // small screen UI adjustments
-                    if (sideNav.classList.contains("mat-drawer-no-nav") === false) {
-                        sideMenuIcon.classList.remove("quartz-invisible");
-                    }
                     sideNavContain.classList.add("mat-drawer-transition");
-                    sideNav.classList.add("mat-drawer-over");
-                    sideNav.classList.remove("mat-drawer-side");
-                    sideNavContent.style.marginLeft = "";
+                    if (sideNavContent !== null) {
+                        sideMenuIcon.classList.remove("quartz-invisible");
+                        sideNavContent.style.marginLeft = "";
+                        sideNav.classList.add("mat-drawer-over");
+                        sideNav.classList.remove("mat-drawer-side");
+                    }
                     ribbonTitle.classList.add("quartz-invisible");
-                    ribbonSubTitle.classList.add("quartz-invisible");
-                    footEl.classList.add("column-count-one");
+                    if (ribbonSubTitle !== null) {
+                        ribbonSubTitle.classList.add("quartz-invisible");
+                    }
                     footLink.classList.add("footer-mobile-down");
                     footLink.classList.remove("footer-down");
+                    footEl = document.querySelector(".footer-down, .footer-mobile.down");
+                    footEl.classList.add("column-count-one");
                 }
+                tabGroups.forEach(function (container) {
+                    const tabs = container.querySelectorAll('[role="tab"]');
+
+                    const totalTabsWidth = Array.from(tabs).reduce(function (sum, tab) {
+                        return sum + tab.getBoundingClientRect().width;
+                    }, 0);
+
+                    const tablistWidth = container.getBoundingClientRect().width;
+
+                    if (totalTabsWidth > tablistWidth) {
+                        container.classList.add("mat-mdc-tab-header-pagination-controls-enabled");
+                    } else {
+                        container.classList.remove("mat-mdc-tab-header-pagination-controls-enabled");
+                    }
+                });
             };
 
-        if (window.innerWidth < 768) {
+        if (globalThis.innerWidth < 768) {
             altLangLnk.textContent = "fr";
         } else {
             altLangLnk.textContent = "Français";
         }
-        if (window.innerWidth < 960) {
-            showHideNav(false);
-            if (sideNav.classList.contains("mat-drawer-opened") === true && sideNav.classList.contains("mat-drawer-no-nav") === false) {
+        if (globalThis.innerWidth < 960) {
+            smallPageView(false);
+            if (sideNav.classList.contains("mat-drawer-opened") === true && document.querySelector("mat-sidenav-content") !== null) {
                 backdropEl.classList.add("mat-drawer-shown");
                 backdropEl.addEventListener("click", hideSideNav, false);
             }
         } else {
-            showHideNav(true);
-            if (sideNav.classList.contains("mat-drawer-opened") === true) {
+            smallPageView(true);
+            if (sideNav.classList.contains("mat-drawer-opened") === true && document.querySelector("mat-sidenav-content") !== null) {
                 backdropEl.classList.remove("mat-drawer-shown");
                 backdropEl.removeEventListener("click", hideSideNav, false);
             }
         }
-    },
+    }, 
     showLoginMenu = function () {
-        var menuTrigger = document.querySelector(".mat-mdc-menu-trigger"),
+        let menuTrigger = document.querySelector(".mat-mdc-menu-trigger"),
             hideprofileMenu = function () {
                 showProfileMenu(true);
             },
@@ -202,7 +225,7 @@ var anchorUri,
                     overlayBG.classList.remove("quartz-invisible");
                     backdropTrigger.addEventListener("click", hideprofileMenu, false);
                     profileMenu.style.top = accountBtnPos.bottom + "px";
-                    if (window.innerWidth < 960) {
+                    if (globalThis.innerWidth < 960) {
                         profileMenu.style.left = accountBtnPos.left - 118.5 + "px";
                     } else {
                         profileMenu.style.left = accountBtnPos.left + "px";
@@ -216,12 +239,75 @@ var anchorUri,
         } else {
             showProfileMenu(false);
         }
+    }, 
+    gotoPage = function () {
+        globalThis.location.href = this.value;
     },
-    tabActivate = function () {
-        let tabContent,
-            tabGroup = this.closest("quartz-tabs"),
-            tabHeadArr = tabGroup.querySelectorAll("[role='tab']"),
-            tabBodyArr = tabGroup.querySelectorAll("mat-tab-body");
+    openDialog = function () {
+        let overlayElm = this.popoverTargetElement, 
+            overlayWrap = overlayElm.closest(".cdk-global-overlay-wrapper"),
+            closeButton = overlayElm.querySelector("quartz-secondary-button"),
+            closeIcon = overlayElm.querySelector("quartz-icon-button"),
+            
+            htmlTag = document.getElementsByTagName("html")[0],
+            appRootTag = document.getElementsByTagName("app-root")[0],
+            backdropTrigger = document.querySelector(".cdk-overlay-container"),
+            overlayBG = document.querySelector(".cdk-overlay-dark-backdrop"),
+            endDialog = function () {
+                htmlTag.classList.remove("cdk-global-scrollblock");
+                htmlTag.style.left = "";
+                htmlTag.style.top = "";
+                appRootTag.ariaHidden = false;
+                overlayElm.style.width = "";
+                overlayElm.style.minWidth = "";
+                overlayElm.style.position = "";
+                overlayElm.classList.add("quartz-invisible");
+                overlayWrap.classList.add("quartz-invisible");
+                backdropTrigger.classList.add("quartz-invisible");
+                overlayBG.classList.add("quartz-invisible");
+                closeButton.removeEventListener("click", endDialog, false);
+                closeIcon.removeEventListener("click", endDialog, false);
+            };
+
+        if (htmlTag.classList.contains("cdk-global-scrollblock") === false) {
+            htmlTag.classList.add("cdk-global-scrollblock");
+            htmlTag.style.left = "0px;";
+            htmlTag.style.top = "0px;";
+            appRootTag.ariaHidden = true;
+            overlayElm.style.width = "90%";
+            overlayElm.style.minWidth = "598px";
+            overlayElm.style.position = "static";
+            overlayElm.classList.remove("quartz-invisible");
+            overlayWrap.classList.remove("quartz-invisible");
+            backdropTrigger.classList.remove("quartz-invisible");
+            overlayBG.classList.remove("quartz-invisible");
+            closeButton.addEventListener("click", endDialog, false);
+            closeIcon.addEventListener("click", endDialog, false);
+        }
+    },
+    isSectionValid = function (currentStep) {
+        const currentForm = currentStep.querySelector("form");
+        let isFormValid;
+
+        if (currentForm !== null) {
+            isFormValid = currentForm.checkValidity();
+            if (isFormValid === false) {
+                currentForm.reportValidity();
+            }
+            return isFormValid;
+        }
+        return true; // Return true if no form exists in this section
+    }, 
+    stepperNext = function (currentStep, nextSection) {
+//        console.log("Next");
+    }, 
+    stepperBack = function (currentStep, priorSection) {
+//        console.log("Back");
+    };
+
+class tabActivate {
+    constructor() {
+        let tabContent, tabGroup = this.closest("quartz-tabs"), tabHeadArr = tabGroup.querySelectorAll("[role='tab']"), tabBodyArr = tabGroup.querySelectorAll("mat-tab-body");
 
         for (let i = 0; i < tabHeadArr.length; i++) {
             if (tabHeadArr[i] !== this) {
@@ -250,94 +336,47 @@ var anchorUri,
                 }
             }
         }
-    },
-    gotoPage = function () {
-        if (this.type === "submit" && this.formTarget !== "") {
-            window.open(this.value, this.formTarget);
-        } else {
-            window.location.href = this.value;
-        }
-    },
-    openDialog = function () {
-        let overlayWrap = this.popoverTargetElement.closest(".cdk-global-overlay-wrapper"),
-            closeButton = this.popoverTargetElement.querySelector("quartz-secondary-button"),
-            closeIcon = this.popoverTargetElement.querySelector("quartz-icon-button"),
-            htmlTag = document.getElementsByTagName("html")[0],
-            appRootTag = document.getElementsByTagName("app-root")[0],
-            backdropTrigger = document.querySelector(".cdk-overlay-container"),
-            overlayBG = document.querySelector(".cdk-overlay-dark-backdrop"),
-            endDialog = function () {
-                htmlTag.classList.remove("cdk-global-scrollblock");
-                htmlTag.style.left = "";
-                htmlTag.style.top = "";
-                appRootTag.ariaHidden = false;
-                overlayWrap.classList.add("quartz-invisible");
-                backdropTrigger.classList.add("quartz-invisible");
-                overlayBG.classList.add("quartz-invisible");
-                closeButton.removeEventListener("click", endDialog, false);
-                closeIcon.removeEventListener("click", endDialog, false);
-            };
+    }
+}
 
-        if (htmlTag.classList.contains("cdk-global-scrollblock") === false) {
-            htmlTag.classList.add("cdk-global-scrollblock");
-            htmlTag.style.left = "0px;";
-            htmlTag.style.top = "0px;";
-            appRootTag.ariaHidden = true;
-            overlayWrap.classList.remove("quartz-invisible");
-            backdropTrigger.classList.remove("quartz-invisible");
-            overlayBG.classList.remove("quartz-invisible");
-            closeButton.addEventListener("click", endDialog, false);
-            closeIcon.addEventListener("click", endDialog, false);
-        }
-    };
-
-
-for (let i = 0; i < navLink.length; i++) {
-    if (navLink[i].classList.contains("quartz-expand-sidenav-button") === true) {
-        navLink[i].addEventListener("click", accordionActivate, false);
+for (let navLinkElm of navLink) {
+    if (navLinkElm.classList.contains("quartz-expand-sidenav-button") === true) {
+        navLinkElm.addEventListener("click", accordionActivate, false);
     } else {
-        navLink[i].addEventListener("click", selectSideElm, false);
+        navLinkElm.addEventListener("click", selectSideElm, false);
     }
 }
 
-if (typeof contentSection[0] !== "undefined" && contentSection[0].getElementsByTagName("mat-expansion-panel").length !== 0) {
-    accordions = contentSection[0].getElementsByTagName("mat-expansion-panel");
+for (let accordion of accordions) {
+    expandHideBtn = accordion.getElementsByTagName("mat-panel-title");
+    expandHideBtn[0].addEventListener("click", accordionActivate, false);
 }
 
-if (typeof accordions !== "undefined") {
-    for (let i = 0; i < accordions.length; i++) {
-        expandHideBtn = accordions[i].getElementsByTagName("mat-panel-title");
-        expandHideBtn[0].addEventListener("click", accordionActivate, false);
-    }
+for (let tab of tabs) {
+    tab.addEventListener("click", tabActivate, false);
 }
 
-for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener("click", tabActivate, false);
+for (let LinkBtn of linkBtns) {
+    LinkBtn.addEventListener("click", gotoPage, false);
 }
 
-for (let i = 0; i < linkBtns.length; i++) {
-    linkBtns[i].addEventListener("click", gotoPage, false);
+for (let printBtn of printBtns) {
+    printBtn.addEventListener("click", function() {print();}, false);
 }
 
-for (let i = 0; i < printBtns.length; i++) {
-    printBtns[i].addEventListener("click", function() {print()}, false);
-}
-
-for (let i = 0; i < dialogs.length; i++) {
-    dialogs[i].addEventListener("click", openDialog, false);
+for (let dialog of dialogs) {
+    dialog.addEventListener("click", openDialog, false);
 }
 
 sideMenuBtn[0].addEventListener("click", showHideMenu, false);
-if (sideNav.classList.contains("mat-drawer-no-nav") === false) {
-    sideNav.classList.add("mat-drawer-opened");
-    showHideMenu(true);
-}
+sideNav.classList.add("mat-drawer-opened");
+showHideMenu(true);
 
 if (accountBtn !== null) {
     accountBtn.addEventListener("click", showLoginMenu, false);
 }
 
-window.onresize = reSizeAction;
+globalThis.onresize = reSizeAction;
 reSizeAction();
 
 anchorUri = document.location.hash;
@@ -352,12 +391,47 @@ if (anchorUri) {
         }
     }
 }
+
+stepContainer.forEach(function (currentStep, index) {
+    // Find nested "Next" buttons matching the exact class list
+    const nextButtons = currentStep.querySelectorAll("button.mdc-button.mat-mdc-button-base.quartz-button.quartz-primary-button.mat-mdc-button.mat-unthemed"), 
+        backButtons = currentStep.querySelectorAll("button.mdc-button.mat-mdc-button-base.quartz-button.quartz-link-button.mat-mdc-button.mat-unthemed");
+
+    nextButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            let nextSection;
+
+            if (isSectionValid(currentStep) === true) {
+                nextSection = stepContainer[index + 1];
+                if (typeof nextSection !== "undefined") {
+                    stepperNext(currentStep, nextSection);
+                }
+            }
+        });
+    });
+
+    // Find nested "Back" buttons matching the exact class list
+
+    backButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+           let priorSection;
+
+            if (isSectionValid(currentStep) === true) {
+                priorSection = stepContainer[index - 1];
+                if (typeof priorSection !== "undefined") {
+                    stepperBack(currentStep, priorSection);
+                }
+            }
+        });
+    });
+});
 /*
 
 Accordion (done)
-Tabs (done)
 Dialog (done)
 Sub Ribbon (done)
+Tabs (in progress)
+stepper (in progress)
 
 multi column
 Grid
